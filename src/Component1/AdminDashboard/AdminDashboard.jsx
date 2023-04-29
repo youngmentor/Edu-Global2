c72f869eb3ed215f30f7ea1bf6097b240b5855d6
youngmentor <olaleresuliton43@gmail.com>
2023-04-29 19:37:58 +0100

    created the two section

--- src/Component1/AdminDashboard/AdminDashboard.jsx
+++ src/Component1/AdminDashboard/AdminDashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import "./AdminDashboard.css"
 const AdminDashboard = () => {
   return (
-    <div>
-      <div>
+    <div className='AdminDashboard_Main'>
+      <div className='AdminDasboard_Main_Left'>
         <h1>i am the side bar</h1>
       </div>
-      <div>
+      <div className='AdminDashboard_Main_Right'>
         <h1>i am the main content</h1>
       </div>
     </div>
