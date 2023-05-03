b7eb2d4e4a6e4897fab63c554be1801a9e9fee07
youngmentor <olaleresuliton43@gmail.com>
2023-05-03 14:12:46 +0100

    new commit

--- src/Component1/AdminDashboard/AdminDashboardLeft.jsx
+++ src/Component1/AdminDashboard/AdminDashboardLeft.jsx
@@ -2,34 +2,41 @@ import React, { useState } from 'react'
 import Dashboard from './DashboardMain/Dashboard'
 import { useNavigate } from 'react-router-dom'
 import { AiFillHome, AiOutlineProfile } from "react-icons/ai";
-import { MdOutlineClass } from "react-icons/md";
+import { MdOutlineClass,MdCoPresent } from "react-icons/md";
 import { SlArrowDown } from "react-icons/sl";
 import { IoIosArrowForward } from "react-icons/io";
 const AdminDashboardLeft = () => {
   const navigate = useNavigate()
   const [isOpen, setIsOpen] = useState(false)
   const [toggle, setToggle] = useState(false)
-  const ClassDrop =(
+  const ClassDrop = (
     <>
-       {isOpen &&(
-       <div>
-        
-         <p onClick={() => navigate("/admin/classes/newclass/")}>All class</p>
-        <p onClick={() => navigate("/admin/classes/addclass")}>Add class</p>
-       </div>
+      {isOpen && (
+        <div className='AdminDashboardLeft_ClassDrop'>
 
-       ) }
+          <p onClick={() => navigate("/admin/classes/newclass/")}>All class</p>
+          <p onClick={() => navigate("/admin/classes/addclass")}>Add class</p>
+        </div>
+
+      )}
     </>
   )
   return (
-    <div>
-        <h1 onClick={() => navigate("/admin")}><AiFillHome/>Dashboard</h1>
-       <div>
-       <h1 onClick={() => navigate("/admin/classes/")}><MdOutlineClass/>classes</h1>
-       {isOpen ? < IoIosArrowForward onClick={() => { setIsOpen(!isOpen) }} /> : <SlArrowDown onClick={() => { setIsOpen(!isOpen) }} />}
+    <div className='AdminDashboardLeft_Main_Wrap'>
+       <div className='AdminDashboardLeft_Dashboard'>
+       <AiFillHome />
+      <p onClick={() => navigate("/admin")}>Dashboard</p>
        </div>
-       {isOpen && ClassDrop}
-        <h1 onClick={() => navigate("/admin/attendance/")}>Attendance</h1>
+      <div className='AdminDashboardLeft_Class'>
+        <MdOutlineClass />
+        <p onClick={() => navigate("/admin/classes/")}>Class</p>
+        {isOpen ? < IoIosArrowForward onClick={() => { setIsOpen(!isOpen) }} style={{fontSize:20}} /> : <SlArrowDown onClick={() => { setIsOpen(!isOpen) }} style={{fontSize:20}} />}
+      </div>
+      {isOpen && ClassDrop}
+      <div className='AdminDashboardLeft_Attendance'>
+        <MdCoPresent/>
+      <p onClick={() => navigate("/admin/attendance/")}>Attendance</p>
+      </div>
     </div>
   )
 }
